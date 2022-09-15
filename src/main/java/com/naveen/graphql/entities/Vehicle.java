bed54ef8364687483f53937557c53c99b1396e35
Naveen <naveen.chaurasia861@gmail.com>
2022-09-15 06:21:31 +0530

    added VehicleQueryresolver and service

--- src/main/java/com/naveen/graphql/entities/Vehicle.java
+++ src/main/java/com/naveen/graphql/entities/Vehicle.java
@@ -24,7 +24,51 @@
 	    @Column(name = "model_code", nullable = false)
 	    private String modelCode;
 
-	    @Column(name = "brand_name")
+	    public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getType() {
+			return type;
+		}
+
+		public void setType(String type) {
+			this.type = type;
+		}
+
+		public String getModelCode() {
+			return modelCode;
+		}
+
+		public void setModelCode(String modelCode) {
+			this.modelCode = modelCode;
+		}
+
+		public String getBrandName() {
+			return brandName;
+		}
+
+		public void setBrandName(String brandName) {
+			this.brandName = brandName;
+		}
+
+		public LocalDate getLaunchDate() {
+			return launchDate;
+		}
+
+		public void setLaunchDate(LocalDate launchDate) {
+			this.launchDate = launchDate;
+		}
+
+		public static long getSerialversionuid() {
+			return serialVersionUID;
+		}
+
+		@Column(name = "brand_name")
 	    private String brandName;
 
 	    @Column(name = "launch_date")
--- src/main/java/com/naveen/graphql/queries/VehicleQuery.java
+++ src/main/java/com/naveen/graphql/queries/VehicleQuery.java
@@ -1,5 +1,26 @@
 package com.naveen.graphql.queries;
 
-public class VehicleQuery {
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.naveen.graphql.entities.Vehicle;
+import com.naveen.graphql.services.VehicleService;
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class VehicleQuery implements GraphQLQueryResolver {
+
+    @Autowired
+    private VehicleService vehicleService;
+
+    public List<Vehicle> getVehicles(final int count) {
+        return this.vehicleService.getAllVehicles(count);
+    }
+
+    public Optional<Vehicle> getVehicle(final int id) {
+        return this.vehicleService.getVehicle(id);
+    }
 }
